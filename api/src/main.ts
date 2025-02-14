07b9f3683bd4ead27aa86d27e03c6ed8f331b93f
pidberezna <Inspection321@gmail.com>
2025-02-14 16:55:14 +0100

    Update main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,7 +8,7 @@ dotenv.config({ path: '.env' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://rently-app-project.onrender.com',
     credentials: true,
   });
   app.use(cookieParser());
